9bc4dca7d9116acdb10a484a648ca57f2584883e
rupeshkumar <rupeshkumar9312@gmail.com>
2023-01-05 11:34:03 +0530

    wip

---
+++ first-app/src/main/java/com/learning/fse/firstapp/FirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.fse.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApplication.class, args);
+	}
+
+}
