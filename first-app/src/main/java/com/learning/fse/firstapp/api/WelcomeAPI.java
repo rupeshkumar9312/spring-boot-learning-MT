a58d1d3beb4c233efaf17870731c0e89e7186c05
rupeshkumar <rupeshkumar9312@gmail.com>
2023-01-05 14:24:46 +0530

    date-api

--- first-app/src/main/java/com/learning/fse/firstapp/api/WelcomeAPI.java
+++ first-app/src/main/java/com/learning/fse/firstapp/api/WelcomeAPI.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/api")
 public class WelcomeAPI {
@@ -11,5 +13,10 @@ public class WelcomeAPI {
     @GetMapping("/messages")
     public String getMessage(){
         return "Welcome to Restful web services";
+    }
+
+    @GetMapping("/date")
+    public String getDate(){
+        return new Date().toString();
     }
 }
