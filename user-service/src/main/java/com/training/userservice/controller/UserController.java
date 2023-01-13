38bb2c172de4a5bc2d7dcdf1cf84b234ebdc9055
rupeshkumar <rupeshkumar9312@gmail.com>
2023-01-13 14:33:11 +0530

    changes to Response Entity

--- user-service/src/main/java/com/training/userservice/controller/UserController.java
+++ user-service/src/main/java/com/training/userservice/controller/UserController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.training.userservice.domain.User;
 import com.training.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,33 +19,39 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public User addUser(@RequestBody User user) throws JsonProcessingException {
+    public ResponseEntity<User> addUser(@RequestBody User user) throws JsonProcessingException {
 //        System.out.println("");
-        return  userService.addUser(user);
+        User response = userService.addUser(user);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<User> getUsers(){
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> userList = userService.getAllUsers();
+        return new ResponseEntity<>(userList, HttpStatus.OK);
     }
 
     @PostMapping("/all")
-    public List<User> addAllUsers(@RequestBody List<User> users){
-        return userService.saveAllUsers(users);
+    public ResponseEntity<List<User>> addAllUsers(@RequestBody List<User> users) {
+        List<User> userList = userService.saveAllUsers(users);
+        return new ResponseEntity<>(userList, HttpStatus.OK);
     }
 
     @DeleteMapping("/{userId}")
-    public void deleteUserById(@PathVariable Integer userId){
+    public ResponseEntity<Void> deleteUserById(@PathVariable Integer userId) {
         userService.deleteUserById(userId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{userId}")
-    public User getUserById(@PathVariable Integer userId){
-        return userService.getUserByUserId(userId);
+    public ResponseEntity<User> getUserById(@PathVariable Integer userId) {
+        User user = userService.getUserByUserId(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PutMapping("/{userId}")
-    public User updateUser(@RequestBody User user,@PathVariable Integer userId){
-        return userService.updateUserById(userId,user);
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Integer userId) {
+        User response = userService.updateUserById(userId, user);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
